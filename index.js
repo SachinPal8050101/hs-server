e31fd161457c818444ef09bb42225abd1de73f89
SachinPal8050101 <sachin.kumar1@daffodilsw.com>
2023-05-03 13:22:43 +0530

    serve till html content

--- index.js
+++ index.js
@@ -1,13 +1,49 @@
-const express = require('express');
-const app = express();
+// Import the required modules
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
+// Define the server's hostname and port
+const hostname = '127.0.0.1';
+const port = 3000;
 
-app.use('/',express.static('.well-known'));
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+// Helper function to serve a file
+function serveFile(res, filePath, contentType) {
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      res.statusCode = 404;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end('File not found');
+    } else {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', contentType);
+      res.end(data);
+    }
+  });
+}
+
+// Create the server
+const server = http.createServer((req, res) => {
+  console.log(req.url)
+  switch (req.url) {
+    case '/':
+      serveFile(res, path.join(__dirname, 'index.html'), 'text/html');
+      break;
+    case '/.well-known/assetlinks.json':
+      serveFile(res, path.join(__dirname, '.well-known', 'assetlinks.json'), 'application/json');
+      break;
+    case '/.well-known/apple-app-site-association':
+      serveFile(res, path.join(__dirname, '.well-known', 'apple-app-site-association'), 'application/json');
+      break;
+    default:
+      res.statusCode = 404;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end('Not found');
+      break;
+  }
 });
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
+// Start listening for incoming requests
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
-
